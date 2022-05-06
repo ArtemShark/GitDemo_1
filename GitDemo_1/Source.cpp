a9b3e14ba05efe971c6f0e067342112456baab72
Artem <artem1podderegin@gmail.com>
2022-05-06 19:20:12 +0200

    Implementation of calculator functions

--- GitDemo_1/Source.cpp
+++ GitDemo_1/Source.cpp
@@ -3,3 +3,16 @@
 int addition(int num1, int num2) {
 	return num1 + num2;
 }
+
+int substraction(int num1, int num2) {
+	return num1 - num2;
+}
+
+int multiplication(int num1, int num2) {
+	return num1 * num2;
+}
+
+int division(int num1, int num2) {
+	return num1 / num2;
+}
+
